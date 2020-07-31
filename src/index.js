493ad0f46bc7c7ddcaeb28fc7defd51324c5eb49
Nathalia <nathalyabezerra@live.com>
2020-07-31 03:51:11 -0300

    adding index+

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Pages/Home';
+import CadastraVideo from './components/Pages/cadastro/Video';
+import CadastraCategoria from './components/Pages/cadastro/Categoria';
+import Pagina404 from './components/Pages/404-error';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+
+      <Route path="/cadastro/video" component={CadastraVideo} />
+      <Route path="/cadastro/categoria" component={CadastraCategoria} />
+      <Route path="/" component={Home} exact />
+
+      <Route component={Pagina404} />
+
+    </Switch>
+
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
