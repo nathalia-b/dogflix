db2ce7629c5bb78da0a5afba9b178f16995b4683
Nathalia <nathalyabezerra@live.com>
2020-08-01 03:03:24 -0300

    padding change

--- src/components/Pages/Home/index.js
+++ src/components/Pages/Home/index.js
@@ -1,99 +1,95 @@
-import React from 'react';
-
-import Menu from '../../Menu';
-
-import dadosIniciais from '../../../data/dados_iniciais.json';
-
+import React, { useEffect, useState} from 'react';
+//import dadosIniciais from '../../../data/dados_iniciais.json';
 import BannerMain from '../../BannerMain';
-
 import Carousel from '../../Carousel';
+import PageDefault from '../../../components/PageDefault';
+//import styled from 'styled-components';
+import categoriasRepository from '../../../repositories/categorias';
+//import loadingimg from '../../../components/PageDefault/loadingimg.gif';
 
-import Footer from '../../Footer';
+/*const Imagem = styled.img`
+    width: 100%;
+    max-width: 490px;
+    border-top: orangered;
+    background: black;
 
-
-
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  
+    position: fixed;
+    border: 12px solid #fff;
+  }`;*/
 function Home() {
+  const [dadosIniciais, setDadosIniciais] = useState([]);
+
+  useEffect(() => {
+    
+    categoriasRepository.getAllWithVideos()
+      .then((categoriasComVideos) => {
+        console.log(categoriasComVideos[0].videos[0]);
+        setDadosIniciais(categoriasComVideos);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
 
   return (
+    
+    <PageDefault paddingAll={0}>
+      {dadosIniciais.length === 0 && (<div>{/*<Imagem className="loadingimg" src={loadingimg} alt="Dogflix logo" />*/}</div>)}
 
-    <div style={{ background: "#141414" }}>
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+                      <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription={dadosIniciais[0].videos[0].description}
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
+              
+            </div>
+            
+          );
+        }
 
-      <Menu />
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
 
-
-
-      <BannerMain 
-
+      {/* <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-
         url={dadosIniciais.categorias[0].videos[0].url}
-
-        videoDescription={"Kakoa e Corky podem te ajudar com isso!"}
-
+        videoDescription="O que"
       />
-
-
-
-      <Carousel 
-
+      <Carousel
         ignoreFirstVideo
-
         category={dadosIniciais.categorias[0]}
-
       />
-
-
-
-      <Carousel 
-
+      <Carousel
         category={dadosIniciais.categorias[1]}
-
       />
-
-
-
-      <Carousel 
-
+      <Carousel
         category={dadosIniciais.categorias[2]}
-
       />
-
-
-
-      {/*<Carousel 
-
+      <Carousel
         category={dadosIniciais.categorias[3]}
-
-      />*/}
-
-
-
-      {/*<Carousel 
-
+      />
+      <Carousel
         category={dadosIniciais.categorias[4]}
-
-      />*/}
-
-
-
-      {/*<Carousel 
-
-        category={dadosIniciais.categorias[5]}
-
-      />*/}
-
-
-
-      <Footer />
-
-
-
-    </div>
-
+      /> */}
+    </PageDefault>
   );
-
 }
-
-
 
 export default Home;
