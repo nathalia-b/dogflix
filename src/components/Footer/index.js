c395d1a1b4706194955aae052611cc50e93bf6ba
Nathalia <nathalyabezerra@live.com>
2020-08-02 15:35:21 -0300

    up Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Nathalia from '../../assets/img/nathalia-b.png'
+import Alura from '../../assets/img/alura.png'
+import Plus from '../../assets/img/plus.png'
 
 function Footer() {
   return (
     <FooterBase>
+      <a href="https://www.github.com/nathalia-b">
+        <img src={Nathalia} width="120" height="30" alt="Logo Alura" />
+      </a>
+      <img src={Plus} width="15" height="15" alt="Logo Alura" />
       <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+        <img src={Alura} width="100" height="40" alt="Logo Alura" />
       </a>
       <p>
         Orgulhosamente criado durante a
