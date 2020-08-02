71a1c14243da975e8162f6c0ec380f72bd68a33b
Nathalia <nathalyabezerra@live.com>
2020-08-02 20:43:17 -0300

    up home

--- src/components/Pages/Home/index.js
+++ src/components/Pages/Home/index.js
@@ -24,7 +24,7 @@ function Home() {
   return (
     
     <PageDefault paddingAll={0}>
-      {dadosIniciais.length === 0 && (<div><center><img className="loadingimg" src={loadingimage} alt="Dogflix logo" /></center></div>)}
+      {dadosIniciais.length === 0 && (<div><center><img className="loadingimg" src={loadingimage} alt="Dogflix loading" /></center></div>)}
 
       {dadosIniciais.map((categoria, indice) => {
         if (indice === 0) {
