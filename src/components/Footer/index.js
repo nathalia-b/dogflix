fd9966752fa24109a08b956b50e2cae5ff21cdec
Nathalia <nathalyabezerra@live.com>
2020-08-02 20:47:07 -0300

    up size

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,11 +8,11 @@ function Footer() {
   return (
     <FooterBase>
       <a href="https://www.github.com/nathalia-b">
-        <img className="Logo1" src={Nathalia} width="100" height="27" alt="Logo Alura" />
+        <img className="Logo1" src={Nathalia} width="100" height="25" alt="nathalia-b" />
       </a>
-      <img className="Logo3" src={Plus} width="15" height="15" alt="Logo Alura" />
+      <img className="Logo3" src={Plus} width="15" height="15" alt="++" />
       <a href="https://www.alura.com.br/">
-        <img className="Logo2" src={Alura} width="95" height="30" alt="Logo Alura" />
+        <img className="Logo2" src={Alura} width="50" height="20" alt="Logo Alura" />
       </a>
       <p>
         Orgulhosamente criado durante a
