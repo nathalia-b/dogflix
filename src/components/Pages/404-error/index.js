1bced47617831bf913e3940234101df8ddc84aa9
Nathalia <nathalyabezerra@live.com>
2020-08-02 11:58:57 -0300

    up 404 page

--- src/components/Pages/404-error/index.js
+++ src/components/Pages/404-error/index.js
@@ -1,37 +1,69 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import PageDefault from '../../PageDefault';
+import PageDefault from '../../PageDefault';
+import ButtonHome from '../404-error/buttonhome.js'
 import '../../../index.css';
+import ImagemDeErro1 from '../../../assets/img/erro1.png';
+import ImagemDeErro2 from '../../../assets/img/erro2.png';
+import ImagemDeErro3 from '../../../assets/img/erro3.png';
+import ImagemDeErro4 from '../../../assets/img/erro4.png';
+
+import styled from 'styled-components';
+
+
+const Imagem = styled.img`
+    width: 100%;
+    max-width: 1010px;
+    border-top: orange;
+    background: red;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  
+    position: center;
+   
+  }`;
+  
+function aux404(ImagemDeErro){
+  return (
+    <PageDefault>
+      
+        <Imagem className="Erro" src={ImagemDeErro} alt="Erro1" />
+      
+      <ButtonHome as={Link} className="ButtonLink" to="/">
+      Voltar
+    </ButtonHome>
+    </PageDefault>
+  );
+}   
+
 
 function Pagina404() {
-  /* const min = Math.ceil(0);
-  const max = Math.floor(2);
-  const a = Math.floor(Math.random() * (max - min + 1)) + min; */
-
-  /* if (a === 0) {
-    return (
-      <PageDefault>
-        <Link to="/">
-          <img className="Erro" src={ImagemDeErro} alt="Erro1" />
-        </Link>
-
-      </PageDefault>
+  const min = Math.ceil(1);
+  const max = Math.floor(4);
+  const i = Math.floor(Math.random() * (max - min + 1)) + min; 
+  
+  console.log(i);
+   if (i === 1) {
+    return(
+          aux404(ImagemDeErro1)    
     );
-  } if (a === 1) {
-    return (
-      <Link to="/">
-        <img className="Erro" src={ImagemDeErro2} alt="Erro2" />
-      </Link>
+  }else if (i === 2) {
+    return(
+          aux404(ImagemDeErro2)    
     );
+  }else if(i === 3){
+    return (
+          aux404(ImagemDeErro3)    
+    );
+  }else{
+    return (
+          aux404(ImagemDeErro4)    
+    ); 
   }
-  return (
-    <Link to="/">
-        <img className="Erro" src={ImagemDeErro3} alt="Erro3" />
-    </Link>
-  ); */
-  return (
-    <Link to="/">Erro.</Link>
-  );
+  
+
 }
 
 export default Pagina404;
