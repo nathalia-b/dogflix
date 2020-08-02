773d8d21ebe1b8040fa60b6cc4c2ca999de5a29d
Nathalia <nathalyabezerra@live.com>
2020-08-02 12:01:39 -0300

    adding buttonhome for 404

---
+++ src/components/Pages/404-error/buttonhome.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+
+const ButtonHome = styled.button`
+   
+    width: 10%;
+    max-width: 900px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    top: 395px; left: 900px;
+    color: var(--white);
+    background: var(--black);
+    border: 1px solid #b0b0ec;
+    box-sizing: border-box;
+    cursor: pointer;
+    padding: 5px 13px;
+   
+    text-decoration: none;
+   
+    transition: opacity .4s;
+  }
+  .ButtonLink:hover,
+  .ButtonLink:focus {
+    opacity: .10;
+
+
+  }
+`;
+export default ButtonHome;
