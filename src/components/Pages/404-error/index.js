fe6eb45ddbe97fcd2eede1b710617954c89393c6
Nathalia <nathalyabezerra@live.com>
2020-08-02 20:42:53 -0300

    up 404 page

--- src/components/Pages/404-error/index.js
+++ src/components/Pages/404-error/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../PageDefault';
-import ButtonHome from '../404-error/buttonhome.js'
 import '../../../index.css';
 import ImagemDeErro1 from '../../../assets/img/erro1.png';
 import ImagemDeErro2 from '../../../assets/img/erro2.png';
 import ImagemDeErro3 from '../../../assets/img/erro3.png';
 import ImagemDeErro4 from '../../../assets/img/erro4.png';
-import ImagemDeErro5 from '../../../assets/img/erro4.png';
+import ImagemDeErro5 from '../../../assets/img/erro5.png';
 
 import styled from 'styled-components';
 
@@ -19,18 +18,19 @@ const Imagem = styled.img`
     display: flex;
     justify-content: center;
     align-items: center;
-    position: center;
-  }`;
+    position: center; 
+
+    @media (max-width: 850px) {
+      padding-top: 100px;
+  }
+  }`
   
 function aux404(ImagemDeErro){
   return (
     <PageDefault>
-      
-        <Imagem className="Erro" src={ImagemDeErro} alt="Erro1" />
-      
-      <ButtonHome as={Link} className="ButtonLink" to="/">
-      Início
-    </ButtonHome>
+        <Link to="/">
+           <Imagem className="Erro" src={ImagemDeErro} alt="Imagem de erro" />
+        </Link>
     </PageDefault>
   );
 }   
@@ -40,7 +40,7 @@ function Pagina404() {
   const min = Math.ceil(1);
   const max = Math.floor(5);
   const i = Math.floor(Math.random() * (max - min + 1)) + min; 
-  
+  //const i = 5;
   console.log(i);
    if (i === 1) {
     return(
