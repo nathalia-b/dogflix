700826c78f214eb1c70ebe654c2503d211abfb40
Nathalia <nathalyabezerra@live.com>
2020-08-02 12:49:28 -0300

    up 404

--- src/components/Pages/404-error/index.js
+++ src/components/Pages/404-error/index.js
@@ -13,7 +13,7 @@ import styled from 'styled-components';
 
 const Imagem = styled.img`
     width: 100%;
-    max-width: 940px;
+    max-width: 800px;
     background: black;
     display: flex;
     justify-content: center;
