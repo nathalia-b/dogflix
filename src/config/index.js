9594785add04760db489b272a0353963b8734cb1
Nathalia <nathalyabezerra@live.com>
2020-08-02 20:43:35 -0300

    up config

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
-  ? 'https://dogflix.herokuapp.com'
-  : 'https://dogflix.herokuapp.com';
+  ? 'http://localhost:8080'
+  : 'https://dogflix.herokuapp.com/';
 
 export default {
   URL_BACKEND,
