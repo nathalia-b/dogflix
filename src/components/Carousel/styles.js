e2b1ffb31b919150ce606f3bdc7599cd29300c5d
Nathalia <nathalyabezerra@live.com>
2020-08-03 16:11:51 -0300

    up

--- src/components/Carousel/styles.js
+++ src/components/Carousel/styles.js
@@ -48,7 +48,7 @@ export const VideoCardList = styled.ul`
 `;
 
 export const VideoCardGroupContainer = styled.section`
-  color: white;
+  color: #e9e8e8;
   min-height: 197px;
   margin-left: 5%;
   margin-bottom: 16px;
